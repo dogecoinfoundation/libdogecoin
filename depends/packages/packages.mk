7538d4c39ca37b90db6ebae1fd4f1bcd90c455c2
fanquake <fanquake@gmail.com>
2021-03-17 12:06:29 +0800

    build: split native_cctools

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -8,5 +8,10 @@ upnp_packages=
 darwin_native_packages = 
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools
+darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
+
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+darwin_native_packages+= native_clang
+endif
+
 endif
