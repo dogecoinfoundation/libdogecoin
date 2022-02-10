c1a61b06fad30b7f89a153ac543c8b58b5219c99
Dakoda Greaves <dakoda@xanimo.net>
2022-02-09 19:46:23 -0800

    build: autoreconf dependencies
    adds basic autoreconf build configuration
    -configure.ac (describes configuration for autoreconf)
    -Makefile.am (describes source of lib files and compiler flags for automake)
    
    updates .gitignore to exclude extraneous build files
    
    adds placeholders for libdogecoin header, libdogecoin-config, utest.h and unittester.c

---
+++ include/dogecoin/dogecoin.h
@@ -0,0 +1,91 @@
+/*
+
+ The MIT License (MIT)
+ Copyright (c) 2022 bluezr
+ Copyright (c) 2022 The Dogecoin Foundation
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#ifndef __LIBDOGECOIN_DOGECOIN_H__
+#define __LIBDOGECOIN_DOGECOIN_H__
+
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef uint8_t dogecoin_bool; //!serialize, c/c++ save bool
+
+#ifndef __cplusplus
+# ifndef true
+#  define true 1
+# endif
+
+# ifndef false
+#  define false 0
+# endif
+#endif //__cplusplus
+
+#ifdef __cplusplus
+# define LIBDOGECOIN_BEGIN_DECL extern "C" {
+# define LIBDOGECOIN_END_DECL	}
+#else
+# define LIBDOGECOIN_BEGIN_DECL /* empty */
+# define LIBDOGECOIN_END_DECL	/* empty */
+#endif
+
+#ifndef LIBDOGECOIN_API
+#if defined(_WIN32)
+#ifdef LIBDOGECOIN_BUILD
+#define LIBDOGECOIN_API __declspec(dllexport)
+#else
+#define LIBDOGECOIN_API
+#endif
+#elif defined(__GNUC__) && defined(LIBDOGECOIN_BUILD)
+#define LIBDOGECOIN_API __attribute__((visibility("default")))
+#else
+#define LIBDOGECOIN_API
+#endif
+#endif
+
+#if defined(_MSC_VER)
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#endif
+
+#define DOGECOIN_ECKEY_UNCOMPRESSED_LENGTH 65
+#define DOGECOIN_ECKEY_COMPRESSED_LENGTH 33
+#define DOGECOIN_ECKEY_PKEY_LENGTH 32
+#define DOGECOIN_ECKEY_PKEY_LENGTH 32
+#define DOGECOIN_HASH_LENGTH 32
+
+#define DOGECOIN_MIN(a,b) (((a)<(b))?(a):(b))
+#define DOGECOIN_MAX(a,b) (((a)>(b))?(a):(b))
+
+LIBDOGECOIN_BEGIN_DECL
+
+typedef uint8_t uint256[32];
+typedef uint8_t uint160[20];
+
+LIBDOGECOIN_END_DECL
+
+#endif // __LIBDOGECOIN_DOGECOIN_H__
