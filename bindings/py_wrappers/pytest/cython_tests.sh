d26859eae8c8ae53b78dfa6b89dd4c4836b9bea7
Dakoda Greaves <dakoda@xanimo.net>
2022-05-20 23:05:54 -0700

    cython: force rebuild w/ --user, set tmp build dir and use virtualenv

--- bindings/py_wrappers/pytest/cython_tests.sh
+++ bindings/py_wrappers/pytest/cython_tests.sh
@@ -2,17 +2,22 @@
 # helper script to install dependencies, clean, build and run cython unit tests:
 
 # install:
+python3 -m pip install --upgrade pip
+python3 -m venv .venv
+source .venv/bin/activate
 python3 -m pip install --upgrade cython
 
 # clean:
 FILE=`pwd`/bindings/py_wrappers/libdogecoin/libdogecoin.c
 if test -f "$FILE"; then
-    rm bindings/py_wrappers/libdogecoin/libdogecoin.c
+    rm bindings/py_wrappers/libdogecoin/libdogecoin.c bindings/py_wrappers/libdogecoin/bindings/libdogecoin.o
 fi
 
 # build:
-python3 bindings/py_wrappers/libdogecoin/setup.py build_ext --build-lib bindings/py_wrappers/pytest/
+python3 bindings/py_wrappers/libdogecoin/setup.py build_ext --build-lib bindings/py_wrappers/pytest/ --build-temp bindings/py_wrappers/libdogecoin/bindings/ --force --user
 
 # # run:
 python3 bindings/py_wrappers/pytest/address_test.py 
 python3 bindings/py_wrappers/pytest/transaction_test.py 
+deactivate
+rm -rf .venv
