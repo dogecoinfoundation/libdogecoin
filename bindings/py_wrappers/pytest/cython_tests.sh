a0f3d0c1a66e633ada7ea65f612c111931984eb7
Dakoda Greaves <dakoda@xanimo.net>
2022-05-21 16:22:20 -0700

    cython: contain build_ext, build_temp paths within py_wrappers dir

--- bindings/py_wrappers/pytest/cython_tests.sh
+++ bindings/py_wrappers/pytest/cython_tests.sh
@@ -9,16 +9,17 @@ python3-dbg -m pip install --upgrade cython pip
 # clean:
 FILE=`pwd`/bindings/py_wrappers/libdogecoin/libdogecoin.c
 if test -f "$FILE"; then
-    rm `pwd`/libdogecoin.c `pwd`/libdogecoin.o
+    rm `pwd`/bindings/py_wrappers/libdogecoin/libdogecoin.c `pwd`/bindings/py_wrappers/libdogecoin/libdogecoin.o
 fi
 
 # build:
-python3-dbg bindings/py_wrappers/libdogecoin/setup.py build_ext --build-lib `pwd`/../pytest/ --build-temp `pwd`/../pytest/ --force --user
+python3-dbg bindings/py_wrappers/libdogecoin/setup.py build_ext --build-lib `pwd`/bindings/py_wrappers/pytest/ --build-temp `pwd`/ --force --user
 
 # # run:
 python3-dbg bindings/py_wrappers/pytest/address_test.py 
 PYTHONMALLOC=debug valgrind --tool=memcheck --leak-check=full --track-origins=yes -s \
---suppressions=`pwd`/valgrind-python.supp \
-python3-dbg -Wd -X dev tracemalloc=5 bindings/py_wrappers/pytest/transaction_test.py 
+--suppressions=`pwd`/bindings/py_wrappers/pytest/valgrind-python.supp \
+--log-file=`pwd`/bindings/py_wrappers/pytest/minimal.valgrind.log \
+python3-dbg -Wd -X tracemalloc=5 bindings/py_wrappers/pytest/transaction_test.py 
 deactivate
 rm -rf .venv
