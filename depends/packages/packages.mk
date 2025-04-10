cda5fe93c47ed3b88a3bd34a4ff7285d4d1ff17e
edtubbs <ectubbs@gmail.com>
2025-04-10 11:10:41 -0500

    depends: make yubikey depends selectable with NO_YUBIKEY
    gitian: added to gitian-descriptor

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=libevent libunistring libyubikey libusb ykpers
+packages:=libevent libunistring
 native_packages := native_ccache
 
 wallet_packages=
@@ -6,6 +6,8 @@ wallet_packages=
 upnp_packages=
 
 darwin_native_packages =
+
+yubikey_packages = libyubikey libusb ykpers
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi
